0134a6dfd92afd5b56dd29bc7d29d96d59459473
BenjaminHPepper <B.H.Pepper@gmail.com>
2020-06-17 20:13:43 -0500

    Update spam_class.py

--- spam_class.py
+++ spam_class.py
@@ -18,5 +18,5 @@ y = np.array(dat[57])
 
 #importlib.reload(spam_functions)
 res_nn = spam_nested_cv.nested_cv(spam_functions.nn_mod, X, y, spam_functions.nn_score, 
-                             spam_functions.nn_outer_perf, cv_k1 = 4, seed = 1)
+                                  spam_functions.nn_outer_perf, cv_k1 = 4, seed = 1)
 np.mean(res_nn['Perf'])
